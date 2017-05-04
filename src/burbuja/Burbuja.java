b1c5038aa45c64ae77fa7303d88b51ffb3ff68ff
alexj <alexj@DESKTOP-RV564FS>
2017-05-04 12:58:13 +0200

    El algoritmo de Ordenación burbuja, va comparando las posiciones del array y las ordena de menos a mayor. Comentado

--- src/burbuja/Burbuja.java
+++ src/burbuja/Burbuja.java
@@ -10,7 +10,8 @@ package burbuja;
  * @author alexj
  */
 public class Burbuja {
-
+    //creamos e inicializamos el array con los numeros a usar y 
+    //la variable que usamos para el bucle while
     static int numeros[] = {10, 7, 21, 32, 4};
     static int cambios = 0;
 
@@ -21,11 +22,12 @@ public class Burbuja {
         comprobarNumero(numeros);
         imprimirNumeros(numeros);
     }
-
+    //metodo que usamos para comprobar los numeros y ordenarlos
     static void comprobarNumero(int numeros[]) {
         while (cambios < numeros.length) {
             for (int i = 0; i < numeros.length-1; i++) {
                 if (numeros[i] > numeros[i + 1]) {
+                    //creo las variables para guardar el valor de las posiciones y sustituirlas entre si
                     int numeroMayor = numeros[i];
                     int numeroMenor = numeros [i+1];
                     numeros[i] = numeroMenor;
@@ -35,6 +37,8 @@ public class Burbuja {
             }
         }
     }
+    
+    //imprimimos el array ya modificado
     static void imprimirNumeros(int numeros[]){
         for(int i = 0; i < numeros.length; i++){
             System.out.print(numeros[i] + "\t");
